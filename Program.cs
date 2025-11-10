259470087a358d914614d727a741e95b1a7be157
byblik-0 <azbx1840az09b.x@gmail.com>
2025-11-10 20:26:04 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+interface IAttack
+{
+    void Attack();
+}
+
+interface IHeal
+{
+    void Heal();
+}
+
+class Warrior : IAttack
+{
+    public void Attack()
+    {
+        Console.WriteLine("Воин атакует мечом!");
+    }
+}
+
+class Mage : IAttack, IHeal
+{
+    public void Attack()
+    {
+        Console.WriteLine("Маг бросает огненный шар!");
+    }
+    public void Heal()
+    {
+        Console.WriteLine("Маг хилит!");
+    }
+}
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        object[] characters = { new Warrior(), new Mage() };
+        Console.WriteLine("Все атакующие персонажи: ");
+        foreach (var character in characters)
+        {
+            if (character is IAttack attacker)
+            {
+                attacker.Attack();
+            }
+        }
+        Console.WriteLine("Все лечащие персонажи: ");
+        foreach (var character in characters)
+        {
+            if (character is IHeal healer)
+            {
+                healer.Heal();
+            }
+        }
+    }
+}
