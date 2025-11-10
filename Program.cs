84cb8311c13623dea5d8cdf63836f47bb2104377
byblik-0 <azbx1840az09b.x@gmail.com>
2025-11-10 20:20:43 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,63 +1,31 @@
 ﻿using System.Text;
-interface IAccountOperations
+interface IAttack
 {
-    void Deposit(decimal amount);
-    void Wirhdraw(decimal amount);
+    void Attack();
 }
 
-interface ITransfer
+interface IHeal
 {
-    void Transfer(BankAccount targetAccount, decimal amount);
+    void Heal();
 }
 
-class BankAccount : IAccountOperations, ITransfer
+class Warrior : IAttack
 {
-    public decimal Balance { get; set; }
-    public BankAccount(decimal balance = 0)
+    public void Attack()
     {
-        Balance = balance;
+        Console.WriteLine("Воин атакует мечом!");
     }
-    public void Deposit(decimal amount)
+}
+
+class Mage : IAttack, IHeal
+{
+    public void Attack()
     {
-        if (amount <= 0)
-        {
-            Console.WriteLine("Сумма должна быть положительной");
-        } else
-        {
-            Balance += amount;
-            Console.WriteLine($"Внесено: {amount}. Новый баланс: {Balance}");
-        }
+        Console.WriteLine("Маг бросает огненный шар!");
     }
-    public void Wirhdraw(decimal amount)
+    public void Heal()
     {
-        if (amount <= 0)
-        {
-            Console.WriteLine("Сумма должна быть положительной");
-        }
-        else if(amount > Balance) 
-        {
-            Console.WriteLine("На счете недостаточно средств");
-        }
-        else
-        {
-            Balance -= amount;
-            Console.WriteLine($"Снято: {amount}. Новый баланс: {Balance}");
-        }
-    }
-    public void Transfer(BankAccount targetAccount, decimal amount)
-    {
-        if (targetAccount == null)
-        {
-            Console.WriteLine("Вы не ввели аккаунт!");
-        }
-        Console.WriteLine($"Происходит попытка перевода на сумму {amount} со счета {this.GetHashCode()} на счет {targetAccount.GetHashCode()}");
-        Wirhdraw (amount);
-        targetAccount.Deposit(amount);
-        Console.WriteLine($"Перевод выполнен успешно. Переведено: {amount}");
-    }
-    public override string ToString()
-    {
-      return $"Баланс счёта: {this.GetHashCode()} : {Balance}";
+        Console.WriteLine("Маг хилит!");
     }
 }
 
@@ -65,20 +33,22 @@ internal class Program
 {
     public static void Main(string[] args)
     {
-      var account = new BankAccount(1000);
-      var account2 = new BankAccount(500);
-        Console.WriteLine("Начальные состояния счетов: ");
-        Console.WriteLine(account);
-        Console.WriteLine(account2);
-        Console.WriteLine("----");
-
-      account.Transfer(account2, 345);
-        Console.WriteLine("----");
-
-        Console.WriteLine("Новые состояние счетов: ");
-        Console.WriteLine(account);
-        Console.WriteLine(account2);
-        Console.WriteLine("----");
-
+        object[] characters = { new Warrior(), new Mage() };
+        Console.WriteLine("Все атакующие персонажи: ");
+        foreach (var character in characters)
+        {
+            if (character is IAttack attacker)
+            {
+                attacker.Attack();
+            }
+        }
+        Console.WriteLine("Все лечащие персонажи: ");
+        foreach (var character in characters)
+        {
+            if (character is IHeal healer)
+            {
+                healer.Heal();
+            }
+        }
     }
 }
